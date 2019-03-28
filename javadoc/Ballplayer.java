307d21017bf5f7eac32a594afbe6434f355c67c1
Stephen <stephen@umw.edu>
2019-03-28 10:15:06 -0400

    Make JavaDoc more complete.

--- javadoc/Ballplayer.java
+++ javadoc/Ballplayer.java
@@ -13,7 +13,8 @@ public class Ballplayer {
      * @param name the real (no nicknames) first and last name of the player.
      * @param uni the most well-known uniform number he played under.
      * @param team the mascot name (not city) of the {@link Team} he is most
-     *   commonly associated with.
+     *   commonly associated with. (Used for display purposes only; multiple
+     *   {@link Team} objects can in fact have the same <tt>Ballplayer</tt>.)
      * @throws NoTeamException if the <tt>team</tt> parameter does not
      *   correspond to the mascot name of any known {@link Team}.
      */
@@ -22,9 +23,9 @@ public class Ballplayer {
     
     /**
      * Returns the player's career batting average, measured as total hits
-     *   divided by total "at bats." If the number of "at bats" is zero,
+     *   divided by total "at bats". If the number of "at bats" is zero,
      *   returns 0.0 rather than give a divide-by-zero error.
-     * @deprecated This method should be eschewed in favor of more recently
+     * @deprecated This method should be avoided in favor of more recently
      *   developed stats such as {@link Ballplayer#getOnBasePercentage} and
      *   {@link Ballplayer#getSluggingPercentage}.
      * @return the batting average on a 0.0-to-1.0 scale.
@@ -33,6 +34,22 @@ public class Ballplayer {
         return 0.0;
     }
 
+    /**
+     * Returns the player's career on-base percentage, measured as (total hits
+     * plus walks) divided by total "at bats". If the number of "at bats" is
+     * zero, returns 0.0 rather than give a divide-by-zero error.
+     * @return the on-base percentage on a 0.0-to-1.0 scale.
+     */
     public double getOnBasePercentage() { return 0.0; }
+
+    /**
+     * Returns the player's career slugging percentage, measured as total bases
+     * divided by total "at bats". (For computing total bases, each single the
+     * player hit counts as one "base," each double counts as two, each triple
+     * three and each home run four.) If the number of "at bats" is zero,
+     * returns 0.0 rather than give a divide-by-zero error. Note that a
+     * slugging "percentage" can be greater than 1.
+     * @return the slugging average on a 0.0-to-4.0 scale.
+     */
     public double getSluggingPercentage() { return 0.0; }
 }
--- javadoc/Team.java
+++ javadoc/Team.java
@@ -1,2 +1,27 @@
 
-public class Team {}
+/**
+ * <p>A <tt>Team</tt> represents a group of {@link Ballplayer}s who play
+ * baseball together, and can compute summary statistics about the group's
+ * past performance. Two <tt>Team</tt> objects are required to run a
+ * single-game simulation (see {@link Simulator#simSingleGame}.) Methods like
+ * {@link Team#getTeamBattingAverage()} and {@link Team#getWonLossRecord()}
+ * can be called to get aggregate information about the team's performance.</p>
+ *
+ * <p>Note that a {@link Ballplayer} can be a member of <i>any number</i> of
+ * teams. (This will happen if a player was traded during his career, for
+ * instance.) In this case, the summary statistics for each <tt>Team</tt>, and
+ * its performance in a simulation, will take place as though that player's
+ * entire career stats applied to <i>each</i> of his teams.</p>
+ *
+ * @author MW
+ */
+public class Team {
+
+    public double getTeamBattingAverage() {
+        return 0.0;
+    }
+
+    public int[] getWonLossRecord() {
+        return new int[]{0,0};
+    }
+}
